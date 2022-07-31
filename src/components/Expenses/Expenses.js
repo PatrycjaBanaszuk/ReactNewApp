19b95f7c256bf314ea21d7c618a311bf393abcf0
Patrycja Banaszuk <pwbanaszuk@gmail.com>
2022-07-31 15:27:52 +0100

    filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ExpenseItem from './ExpenseItem'
 import Card from '../../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 import './Expenses.css'
 
 const Expenses = (props) => {
 
+    // const SelectedFilter = (selectedYear) => {
+
+    // }
+
     return (
         <div>
-            
+            <ExpensesFilter/> 
         <Card className='expenses'>
             <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}></ExpenseItem>
 			<ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}></ExpenseItem>
