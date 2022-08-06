2b2f38d973f68d7d8b48e3d7c2c1ba1cfa820267
Patrycja Banaszuk <pwbanaszuk@gmail.com>
2022-08-06 12:37:36 +0100

    adding dynamic style

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Card from '../../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 import './Expenses.css'
 
 const Expenses = props => {
@@ -17,6 +18,7 @@ const Expenses = props => {
 		<div>
 			<Card className='expenses'>
 				<ExpensesFilter selected={filteredYear} onChangeFilter={FilterChangeHandler} />
+				<ExpensesChart expenses={filteredExpenses}/>
 				<ExpensesList items= {filteredExpenses}/>
 			</Card>
 		</div>
